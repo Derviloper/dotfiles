89a09dcce9014fe8cb4b9725f804c681f41bf67a
Daniel da Silva <daniel.da-silva@smake.com>
2025-08-24 13:28:55 +0200

    Update

--- modules/nixos/k8s/default.nix
+++ modules/nixos/k8s/default.nix
@@ -12,15 +12,11 @@
         hash = "sha256-pIfbHJ4vafOPttJ/4ZupkObWQHl77KeOhFszkc4jkaQ=";
         targetNamespace = "argocd";
         createNamespace = true;
-        values = {
-          # global.domain = "argocd.derviloper.de";
-          # configs.params."server.insecure" = true;
-        };
       };
     };
     manifests = {
-      sealed-secret-key.source = "${config.sops.secrets."sealed-secrets-key.yaml".path}";
-      argo-cd-application-set.source = ./argo-cd-application-set.yaml;
+      sealed-secret-key.source = config.sops.secrets."sealed-secrets-key.yaml".path;
+      applications.source = ./applications.yaml;
     };
   };
 }
